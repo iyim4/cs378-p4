4ecab47aebd825543f3229a634abdea6d9f7f6ce
iyim4 <iyim@utexas.edu>
2025-03-15 23:41:02 -0500

    add more design stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = ({ title, tagline }) => {
   return (
     <div class="align-items-center container-fluid">
-      <img src="./images/Header_Banner_Wordless.jpg" alt={"Volcano Watch banner"} class="banner" />
+      <img src="./images/Header_Banner.jpg" alt={"Volcano Watch banner"} class="banner" />
       <div>
         <h1 class="text-center display-1">{ title }</h1>
         <p class="text-center tagline">{tagline}</p>
