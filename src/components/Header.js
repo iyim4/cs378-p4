01976744a6a14b81fc8333db24a6b1410b813494
iyim4 <iyim@utexas.edu>
2025-03-15 21:17:56 -0500

    attempt to display banner image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import React from 'react';
 const Header = ({ title, tagline }) => {
   return (
     <div class="row align-items-center container-fluid">
-      <img src="./Header_Banner_Wordless.jpg" alt={"Volcano Watch banner"} class="banner" />
+      <img src="./images/Header_Banner_Wordless.jpg" alt={"Volcano Watch banner1"} class="banner" />
+      <img src="Header_Banner_Wordless.jpg" alt={"Volcano Watch banner2"} class="banner" />
+      <img src="./Header_Banner_Wordless.jpg" alt={"Volcano Watch banner3"} class="banner" />
+      <img src="./src/images/Header_Banner_Wordless.jpg" alt={"Volcano Watch banner4"} class="banner" />
       <h1 class="text-center display-1">{ title }</h1>
       <p class="text-center tagline">{tagline}</p>
     </div>
