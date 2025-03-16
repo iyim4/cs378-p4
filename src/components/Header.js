e97250a68b802599587d7a27a5242bcdaaa65052
iyim4 <iyim@utexas.edu>
2025-03-16 00:14:18 -0500

    change header banner image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = ({ title, tagline }) => {
     <div class="align-items-center container-fluid">
       <img src="./images/Header_Banner.jpg" alt={"Volcano Watch banner"} class="banner" />
       <div>
-        <h1 class="text-center display-1">{ title }</h1>
+        {/* <h1 class="text-center display-1">{ title }</h1> */}
         <p class="text-center tagline">{tagline}</p>
       </div>
     </div>
